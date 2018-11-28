8e09d185a80b74fe0abc48f0f13f93f8d27b4c14
TSGreenwood <greenwood.ts@gmail.com>
2018-11-28 13:41:45 -0800

    Here I have the eslint and gitignore files populated correctly and JS file functioning well

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 'use strict';
 //I'll start with my first question
+
 var isInstructor =prompt("Are you an instructor?");
 console.log("isInstructor will tell me if they are an instructor");
+var res = (isInstructor.toUpperCase());
+//console.log(isInstructor.toUpperCase());
+console.log('ensures user response is all caps');
+  if(res == 'YES') {
+//if(isInstructor == 'YES') {
+  document.write('Hello, Instructor!');
+  alert('Hello, Instructor!');
+} else {
+  document.write('Hello teammate or stranger!');
+  alert('Hello, Teammate!');
+}
+//alert('I did not hear you.');
