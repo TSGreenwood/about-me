8c4770264caefb8f5a3de445c03890dec4c0aa74
TSGreenwood <greenwood.ts@gmail.com>
2018-11-28 17:15:31 -0800

    Linked styles css sheet

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@
 //I'll start with my first question
 
 var isInstructor =prompt("Are you an instructor?");
-console.log("isInstructor will tell me if they are an instructor");
+console.log(isInstructor + 'will tell me if they are an instructor');
+
 var res = (isInstructor.toUpperCase());
-//console.log(isInstructor.toUpperCase());
+console.log(isInstructor.toUpperCase());
 console.log('ensures user response is all caps');
-  if(res == 'YES') {
-//if(isInstructor == 'YES') {
+  if(res === 'YES') {
+//if(isInstructor === 'YES') {
   document.write('Hello, Instructor!');
   alert('Hello, Instructor!');
 } else {
